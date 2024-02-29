bedca9a85dc2341be8f1c0db4fbd762fa608fccc
huy <huydevct>
2024-02-29 09:40:11 +0700

    feat: create service provider

---
+++ src/providers/MediaStorageServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Huy\MediaStorage\providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class MediaStorageServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
